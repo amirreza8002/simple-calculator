9d5caf2209a8aef03942d195380512c7e74c8c97
amirreza8002 <119850973+amirreza8002@users.noreply.github.com>
2022-12-26 14:15:41 +0330

    Update source code.py

--- source code.py
+++ source code.py
@@ -1,19 +1,25 @@
-#making a variable for results/outputs, to gather stuff in it
+#so first we make an empty variable so we can put our result inside it
+
 output = 0
 
-# Asking What numbers you want using print command and it will repeat the question if the user doesn't put in a number
+# now we need to ask the user what numbers they want to work with, but since user might misclick and put in something other than a number
+#we put a "while True" function, which will repeat the question forever
+
 while True:
-    #will try to get the number
+
+#"try" is a fuction we use when we expect something goes wrong, this will try what you want, and if it doesn't work, raises an exception
     try:
+        #now we get numbers from user, and use the "float" methode to turn their input to actual numbers, because an input is always a str unless we make it otherwise
         num1 = float(input("Hello, What is your First Number? "))
         num2 = float(input("Your Second Number? "))
-        #will break the loop when user puts the right input(numbers)
+ #now we need to add a break, this will break the loop if user inputs numbers, so the loop doesn't go on forever
         break
-    #if user puts the wrong kinf of input, exception will be rised then loop will repeat the question 
+    #if user puts the wrong kind of input, an exceptaion will be raised that will tell the user what they have done wrong
     except ValueError:
         print("number needs to be a number")
 
-# If selected operation is the following then do the following
+# now we make a function to get ask user what kind of operation they want to operate on those numbers
+#the reason we don't do "try" and "except" here is because we cen't raise a value error, becuase we are not converting the input to numbers
 def op():
     operation = input("Operation (+, -, *, /)?")
     if operation == "+":
@@ -24,10 +30,13 @@ def op():
         output=num1*num2
     elif operation == "/":
         output=num1/num2
-    #if operation input is wrong, repeat the question
+    #if operation input is wrong:
     else:
-        op()
+        #tell the use what they have done wrong:
         print("wrong operation")
+        #repeat the question of operation
+        op()
+
+
 #print the result
 print (output)
-
